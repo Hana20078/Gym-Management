f91d4b1f7ba52a4e0fc6e563c10a8a8ff349b55f
hana <DeLL@DESKTOP-9DORN8I>
2026-03-12 21:55:05 +0200

    switch done

--- Gym Management.cpp
+++ Gym Management.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Member_Management.h" 
+#include "member.h" 
 #include "Trainer_Management.h" 
 #include "Membership_Plan_Management.h" 
 #include "Workout_Program_Management.h" 
@@ -10,11 +10,31 @@
 
 using namespace std;
 int main() {
-	cout << " Hello";
-	cout << " hellllo";
-
-	cout << " Shafik Was here";
-
-
-	cout << "commit and push after";
-}
+	int choice;
+	do {
+		cout << "************************GYM_SYSTEM_MENU***********************" << endl;
+		cout << "1.Register new member" << endl;
+		cout << "2.update member information" << endl;
+		cout << "3.Delete member (if allowed)" << endl;
+		cout << "4.search members details" << endl;
+		cout << "5.Display members details" << endl;
+		cout << "6.Track membership status" << endl;
+		cout << "7.exist" << endl;
+		cin >> choice;
+		switch (choice) {
+		case1:
+			register_member();
+		case 2:
+			update_member();
+		case 3:
+			delete_member();
+		case 4:
+			search_member();
+		case 5:
+			display_member();
+		case 6:
+			track_memberships();
+		case 7:
+			cout << "existing...........";
+		}while (choice != 7);
+	}
