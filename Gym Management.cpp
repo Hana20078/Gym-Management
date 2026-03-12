eb97cb74f7e20f54834fc46a1030f42c6bcd4771
hana <DeLL@DESKTOP-9DORN8I>
2026-03-12 21:48:16 +0200

    pew pew

--- Gym Management.cpp
+++ Gym Management.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include "Member_Management.h" 
+#include "Trainer_Management.h" 
+#include "Membership_Plan_Management.h" 
+#include "Workout_Program_Management.h" 
+#include "Billing_System.h"  
+#include "Attendance_Tracking.h" 
+#include "Reports.h"
+
+
 using namespace std;
 int main() {
 	cout << " Hello";
