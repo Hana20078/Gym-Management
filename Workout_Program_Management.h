7bf4fc37098ce379cceb7f6831d402b64f1baa95
Retaj Nabil Fathy Roshdy Elbasyony <retajelbasiony@gmail.com>
2026-04-12 01:57:41 +0200

    kda na2es el vector bs

--- Workout_Program_Management.h
+++ Workout_Program_Management.h
@@ -1,5 +1,19 @@
-#pragma once
-class Workout_Program_Management
+using namespace std; 
+class workout_programs {
+private:
+	int id, duration;
+	string name, target_goal, exercise_list, assigned_trainer;
+public:
+	workout_programs(int i, int d, string n, string tg, string el, string at);
+	void displayprogram();
+};
+class Exercise
 {
+private:
+	string exercise_name;
+	int sets, reps, rest_time;
+public:
+	Exercise(string en, int s, int r, int rt);
+	void display();
 };
 
