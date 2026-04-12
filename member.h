c89766733f127b53eac9f73969c6b5c3b36e6cda
hana <DeLL@DESKTOP-9DORN8I>
2026-04-12 12:16:50 +0200

    llll

--- member.h
+++ member.h
@@ -1,17 +1,27 @@
-#pragma once
 #include "person.h"
-class member:public person 
+#include "Trainer_Management.h"
+class member:public person
 {
 private:
-
+	int regsetion_date;
+	int expiration_date;
+	Trainer_Management Assigned_trainer;
+	bool status_active;
 
 
 	member() {
+		cout << "enter member regestriation : " << endl;
+		cin >> regsetion_date;
+		cout << "enter expiration date : " << endl;
+		cin>>expiration_date;
+		cout << "enter your assigned trianer : " << endl;
+		cin >> Assigned_trainer;
 
 	}
 
 
 
+	
 
 
 
