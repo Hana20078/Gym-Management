301d7c62e0c90eeade53e55e90a7c3f00fe3c126
Retaj Nabil Fathy Roshdy Elbasyony <retajelbasiony@gmail.com>
2026-04-12 12:27:17 +0200

    ..................

--- Workout_Program_Management.cpp
+++ Workout_Program_Management.cpp
@@ -32,3 +32,75 @@ void Exercise::display() {
 	cout << "Rest time:" << rest_time << endl;
 	cout << "======================================================";
 }
+
+void workout_programs::setid()
+{
+    cout << "write workout's id  " << id;
+	cin >> id;
+}
+
+void workout_programs::setduration()
+{
+	cout << "write workout's duration  " << duration;
+	cin >> duration;
+}
+
+void workout_programs::setname()
+{
+	cout << "write workout's name " << name;
+	cin >> name;
+}
+
+void workout_programs::settarget_goal()
+{
+	cout << "write workout's target goal " << target_goal;
+	cin >> target_goal;
+}
+
+void workout_programs::setexercise_list()
+{
+	cout << "write workout's exercise list " << exercise_list;
+	cin >> exercise_list;
+}
+
+void workout_programs::setassigned_trainer()
+{
+	cout << "write workout's assigned trainer" << assigned_trainer;
+	cin >> assigned_trainer;
+}
+
+int workout_programs::getid()
+{
+	cout << "workout's id is " << id;
+    return id;
+}
+
+int workout_programs::getduration()
+{
+	cout << "workout's duration is " << duration;
+    return duration;
+}
+
+string workout_programs::getname()
+{
+	cout << "workout's name is " << name;
+    return name;
+}
+
+string workout_programs::gettarget_goal()
+{
+	cout << "workout's target goal is " << target_goal;
+    return target_goal;
+}
+
+string workout_programs::getexercise_list()
+{
+	cout << "workout's exercise list is " << exercise_list;
+    return exercise_list;
+}
+
+string workout_programs::getassigned_trainer()
+{
+	cout << "workout's assigned trainer is " << assigned_trainer;
+    return assigned_trainer;
+}
--- Workout_Program_Management.h
+++ Workout_Program_Management.h
@@ -1,3 +1,5 @@
+#pragma once
+#include <string>
 using namespace std; 
 class workout_programs {
 private:
@@ -6,6 +8,18 @@ private:
 public:
 	workout_programs(int i, int d, string n, string tg, string el, string at);
 	void displayprogram();
+    void setid();
+    void setduration();
+    void setname();
+    void settarget_goal();
+    void setexercise_list();
+    void setassigned_trainer();
+    int getid();
+    int getduration();
+    string getname();
+    string gettarget_goal();
+    string getexercise_list();
+    string getassigned_trainer();
 };
 class Exercise
 {
@@ -13,18 +27,6 @@ private:
 	string exercise_name;
 	int sets, reps, rest_time;
 public:
-	void setid();
-	void setduration();
-	void setname();
-	void settarget_goal();
-	void setexercise_list();
-	void setassigned_trainer();
-	int getid();
-	int getduration();
-	string getname();
-	string gettarget_goal();
-	string getexercise_list();
-	string getassigned_trainer();
 	Exercise(string en, int s, int r, int rt);
 	void display();
 };
