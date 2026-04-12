3205c35bdc24e98b9482c444f78e7d4b88ae1f98
Retaj Nabil Fathy Roshdy Elbasyony <retajelbasiony@gmail.com>
2026-04-12 02:37:25 +0200

    ......

--- Membership_Plan_Management.h
+++ Membership_Plan_Management.h
@@ -1,5 +1,20 @@
 #pragma once
 class Membership_Plan_Management
 {
-};
+private:
+	int id;
+	string name;
+public:
+	Membership_Plan_Management(int i,string n);
+	void displaymembershipPlan();
 
+};
+class plan {
+private:
+	int plan_id, duration, Access_level;
+	float price;
+	string Allowed_services, Discount_rules;
+public:
+	plan(int pi, int d, int al, float p, string a, string dr);
+	void displayPlan();
+};
