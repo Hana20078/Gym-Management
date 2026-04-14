972a46c33ee317cb087d5eb995198d76e8c600a3
Retaj Nabil Fathy Roshdy Elbasyony <retajelbasiony@gmail.com>
2026-04-14 21:44:46 +0200

    ..............

--- Reports.h
+++ Reports.h
@@ -1,8 +1,19 @@
 #pragma once
+#include<iostream>
+#include <string>
 
 using namespace std;
 
 class Reports
 {
+public:
+	#workout reports
+	void total_workouts();
+	void count_workouts();
+	#membership plans
+	void expired_memberships();
+	void monthly_revenue();
+	void display_performance();
+
 };
 
